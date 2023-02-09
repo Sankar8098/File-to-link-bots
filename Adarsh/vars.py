d123d3c1d98429c8654078ff6c3e1b9ebff460fa
irfan36bot <116106429+irfan36bot@users.noreply.github.com>
2023-02-09 21:43:47 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -22,7 +22,6 @@ class Var(object):
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1125671241 1809710185").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    MY_PASS = "$Irfan"
     OWNER_USERNAME = "Irfan50786"
     if 'DYNO' in environ:
         ON_HEROKU = True
@@ -36,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = "mongodb+srv://Irfan:786or786@cluster0.2jjhd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "DS_Botz"))
+    DATABASE_URL = "mongodb+srv://DSBotz:DSBotz@cluster0.lbaxiea.mongodb.net/?retryWrites=true&w=majority"
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "+eg-Yb9Z92lc0YzY1)
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
