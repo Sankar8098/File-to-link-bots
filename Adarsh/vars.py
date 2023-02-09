592000f758e6083407c772d06d4d3df473f4360d
irfan36bot <116106429+irfan36bot@users.noreply.github.com>
2023-02-09 19:50:52 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,17 +11,18 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = 10956858
     API_HASH = "cceefd3382b44d4d85be2d83201102b7"
-    BOT_TOKEN = "5325357874:AAEEgEkYAo23arFBbDg89qONslSSh9zSYAU"
+    BOT_TOKEN = "6240905328:AAENHR5K5y_eXSyChU8KYTxUTMjCYryK95g"
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = "-1001574159030"
+    BIN_CHANNEL = "-1001738918633"
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1125671241 1809710185").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
+    MY_PASS = "$Irfan"
     OWNER_USERNAME = "Irfan50786"
     if 'DYNO' in environ:
         ON_HEROKU = True
