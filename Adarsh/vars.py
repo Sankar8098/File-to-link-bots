2d507d6fcc50a1556e41dd6b5ddd13b9c4ede3b2
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-02-12 16:58:51 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,20 +9,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = 10956858
-    API_HASH = "cceefd3382b44d4d85be2d83201102b7"
-    BOT_TOKEN = "6240905328:AAENHR5K5y_eXSyChU8KYTxUTMjCYryK95g"
+    API_ID = 23990433
+    API_HASH = "e6c4b6ee1933711bc4da9d7d17e1eb20"
+    BOT_TOKEN = "5822452610:AAEENVB1jI1cIaqI9Wsxxwl_I9ANSHLaQ20"
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = "-1001738918633"
+    BIN_CHANNEL = "-1001682397310"
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1125671241 1809710185").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5821871362").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = "Irfan50786"
+    OWNER_USERNAME = "Sankar2k02"
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
